3c0144d65bd59b7b75a24315ae87dde38bc5e393
lasnick7 <lasnick711@gmail.com>
2025-08-27 12:43:43 +0300

    lesson 3: applications

--- hexlet_django_blog/settings.py
+++ hexlet_django_blog/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'hexlet_django_blog',
+    'bootstrap4',
+    'django_extensions',
+    'hexlet_django_blog.article',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +58,7 @@ ROOT_URLCONF = 'hexlet_django_blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
